4552ea18a8f414fe47f6dc56feb5d1f1aac46fb5
azureswastka <azureswastika@gmail.com>
2021-05-20 17:19:23 +0300

    Add logger

--- go/main.go
+++ go/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -17,34 +13,9 @@ import (
 
 var staticDir = "./static"
 var htmlDir = staticDir + "/html/"
-
-func index(w http.ResponseWriter, r *http.Request) {
-	html, err := ioutil.ReadFile(htmlDir + "index.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Fprint(w, string(html))
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	html, err := ioutil.ReadFile(htmlDir + "login.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Fprint(w, string(html))
-}
-
-func singleJoiningSlash(a, b string) string {
-	aslash := strings.HasSuffix(a, "/")
-	bslash := strings.HasPrefix(b, "/")
-	switch {
-	case aslash && bslash:
-		return a + b[1:]
-	case !aslash && !bslash:
-		return a + "/" + b
-	}
-	return a + b
-}
+var origin, _ = url.Parse(host())
+var path = "/api/*catchall"
+var reverseProxy = httputil.NewSingleHostReverseProxy(origin)
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -55,52 +26,15 @@ func init() {
 func main() {
 	router := mux.NewRouter()
 
-	host := func() string {
-		val, ok := os.LookupEnv("api_host")
-		if !ok {
-			return "http://127.0.0.1:8000/"
-		}
-		return val
-	}
-
-	origin, _ := url.Parse(host())
-
-	path := "/api/*catchall"
-
-	reverseProxy := httputil.NewSingleHostReverseProxy(origin)
-	reverseProxy.Director = func(r *http.Request) {
-		r.Header.Add("X-Forwarded-Host", r.Host)
-		r.Header.Add("X-Origin-Host", origin.Host)
-		r.URL.Scheme = origin.Scheme
-		r.URL.Host = origin.Host
-
-		wildcardIndex := strings.IndexAny(path, "*")
-		proxyPath := singleJoiningSlash(origin.Path, r.URL.Path[wildcardIndex:])
-		if strings.HasSuffix(proxyPath, "/") && len(proxyPath) > 1 {
-			proxyPath = proxyPath[:len(proxyPath)-1]
-		}
-		r.URL.Path = proxyPath
-	}
+	reverseProxy.Director = Director
 
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
 	router.HandleFunc("/", index)
 	router.HandleFunc("/login", login)
-	router.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
-		c := http.Cookie{
-			Name:   "Authorization",
-			MaxAge: -1}
-		http.SetCookie(w, &c)
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	})
-	router.PathPrefix("/api/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("Authorization")
-		if err != nil {
-			reverseProxy.ServeHTTP(w, r)
-			return
-		}
-		r.Header.Add(cookie.Name, cookie.Value)
-		reverseProxy.ServeHTTP(w, r)
-	})
+	router.HandleFunc("/logout", logout)
+	router.PathPrefix("/api/").HandlerFunc(apiReverseProxy)
+
+	router.Use(RequestLoggerMiddleware(router))
 
 	server := &http.Server{
 		Handler:      router,
